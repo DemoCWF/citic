a731270bff270544c82cef5f7e1ae12aba9c3011
chenchaowu <1466358244@qq.com>
2019-12-16 18:19:08 +0800

    fix：Goods相关.

--- src/main/java/com/citic/demo/mapper/GoodsInfoMapper.java
+++ src/main/java/com/citic/demo/mapper/GoodsInfoMapper.java
@@ -2,7 +2,21 @@ package com.citic.demo.mapper;
 
 import com.citic.demo.base.IBaseDao;
 import com.citic.demo.entity.GoodsInfo;
+import com.citic.demo.query.GoodsQuery;
+import com.citic.demo.request.GoodsRequest;
+
+import java.util.List;
 
 public interface GoodsInfoMapper extends IBaseDao<GoodsInfo> {
+
+    List<GoodsInfo> query(GoodsQuery goodsQuery) throws Exception;
+
+    GoodsInfo queryGoodsInfoById(String primaryKey) throws Exception;
+
+    int saveGoodsInfo(GoodsRequest goodsRequest) throws Exception;
+
+    int updateGoodsInfo(GoodsRequest goodsRequest) throws Exception;
+
+    int deleteGoodsInfo(String primaryKey) throws Exception;
 
 }
